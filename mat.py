9d0a1e27106dc749fa030736544f35941c975d8e
George Kovar <kovargeorge@gmail.com>
2020-11-11 15:35:48 +0100

    update

---
+++ mat.py
@@ -0,0 +1,7 @@
+"""
+This file contains all the properties for the material that we select
+"""
+
+rho = 1.225  # material density
+E = 270e9  # modulus
+sigma_y = 200  # [Pa] yield stress
---
+++ temp.py
@@ -0,0 +1,3 @@
+from mat import *
+
+print(rho)
