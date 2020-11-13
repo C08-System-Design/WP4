9b9de9f495ac8d3416d80756fb5ac77da1934132
tompson-07 <74308132+tompson-07@users.noreply.github.com>
2020-11-13 09:35:00 +0100

    Launch Load calculation & launch and satellite characterisitcs
    A parametric file for the launch load calculation which extracts all parameters from a dedicated file. No changes have to be made to the calculator if parameters change in the dedicated file.

---
+++ launch_loads.py
@@ -0,0 +1,18 @@
+#############################################################################################################################################
+# All values ar retrieved from launch data parameterically so no changes have to be made in this file if a parameter in launch load changes #
+#############################################################################################################################################
+
+import math
+from launch_data import *
+
+m = 0.5 * m
+
+def load(m, g, Load, FLL):
+    force = m * g * Load * FLL
+    return force
+
+long_force = load(m, g, long_load, FLL)     # Longitudinal Force
+lat_force = load(m, g, lat_load, FLL)       # Lateral Force
+
+# print(long_force)
+# print(lat_force)
