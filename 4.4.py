a007716d1cd246a8d56317a72243572c19a21255
luke-shu <31172194+luke-shu@users.noreply.github.com>
2020-11-18 14:07:02 +0100

    added some comment

--- 4.4.py
+++ 4.4.py
@@ -1,19 +1,28 @@
 import math
 #this file will calculate the fasterner pattern
+#hole locations
 z_i = [0.03, -0.03]     #count from largest positive hole
 x_i = [0.08]    #count positive from side
-w = 0.1
-length = 0.5
+
+#base plate geometry
+w = 0.1         #width of base plate
+length = 0.5    #length of base plate
+t2 = 0.01   #thickness of base plate
+
+#margin to edge
 top = w-z_i[0]
 side = length/2 -x_i[0]
 print top,side
-t2 = 0.01   #thickness of base plate
 col = len(x_i)  #column each side
 row = len(z_i)  #holes per column
 n = col * row   #hole each side
+
+#material property
 TYS = 276000000  #Tensile Yield Stress
 ratio = 0.55
 SYS = ratio * TYS   #Shear Yield Stress
+
+#margins
 SF = 2  #safety factor
 SC = 3  #stress concentration
 Pi = math.pi
