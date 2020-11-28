2981bd8013d72a28c666ed2de964e99af944019a
RypkeP <oprschreurs@gmail.com>
2020-11-28 14:45:50 +0100

    Update Resultand_load.py

--- Resultand_load.py
+++ Resultand_load.py
@@ -1,10 +1,8 @@
 from math import *
 from sympy import *
 from Thermal_orbit import Loads_therm
-from launch_loads import F_x
+from launch_loads import F_y
 
-# F_x is also F_y so I'm converting it for clarity
-F_y = F_x
 # Calculating the resulting force due to thermal stresses and lateral stresses
 
 F_yres = F_y + Loads_therm[0][0]
