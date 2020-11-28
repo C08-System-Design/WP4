92b51502215b074d70b5c71d6b08b1dc5479f8a1
George Kovar <kovargeorge@gmail.com>
2020-11-28 16:55:30 +0100

    Update push_fail.py

--- push_fail.py
+++ push_fail.py
@@ -2,36 +2,39 @@ from mat import *
 from push_check import max_Fyi
 from push_check import *
 
-def get_sigma (Fy, A_zx):
+
+def get_sigma(Fy, A_zx):
     sigma_xz = Fy / A_zx  # normal stress due to Fyi
     return sigma_xz
 
-def get_tau (Fy, A_y):
+
+def get_tau(Fy, A_y):
     tau_y = Fy / A_y  # shear force that counters normal stress (due to Fyi)
     return tau_y
 
-Fy = max_Fyi #use maximum loaded fastener
+
+Fy = max_Fyi  # use maximum loaded fastener
 
 
-#values need to be put in still
+# values need to be put in still
 for config in configs:
     D_fo = config[2]+config[2]*0.4
     D_fi = config[2]
-    t2 = 0.001  #thickness from spacecraft wall
-    t3 = 0.001  #thickness from lug wall
-    sigma_yield = A7075_T6.get(sigma_y)  #depends on material type
-    tau_yield = A7075_T6.get(tau_y)  #depends on material type
+    t2 = 0.001  # thickness from spacecraft wall
+    t3 = 0.001  # thickness from lug wall
+    sigma_yield = A7075_T6.get(sigma_y)  # depends on material type
+    tau_yield = A7075_T6.get(tau_y)  # depends on material type
 
-    A_zx = 1/4 * pi *(D_fo**2 - D_fi**2) #area normal stress works on
-    A_y2 = pi * D_fi * t2 #area shear stress works on with thickness t2
-    A_y3 =  pi * D_fi * t2 #area shear stress works on with thickness t3 (shear stress is calculated for both plates on their own)
+    A_zx = 1/4 * pi *(D_fo**2 - D_fi**2) # area normal stress works on
+    A_y2 = pi * D_fi * t2  # area shear stress works on with thickness t2
+    A_y3 =  pi * D_fi * t2  # area shear stress works on with thickness t3 (shear stress is calculated for both plates on their own)
 
 
-    sigma_xz = get_sigma(Fy, A_zx) #calc norm stress
-    tau_y_2 = get_tau(Fy, A_y2)    #calc shear stress for back plate
-    tau_y_3 = get_tau(Fy, A_y3)    #calc shear stress for lug plate
+    sigma_xz = get_sigma(Fy, A_zx) # calc norm stress
+    tau_y_2 = get_tau(Fy, A_y2)    # calc shear stress for back plate
+    tau_y_3 = get_tau(Fy, A_y3)    # calc shear stress for lug plate
 
-    #check if shear stresses are too high
+    # check if shear stresses are too high
     if tau_y_2 > tau_yield:
         print("the shear stress is too high for thickness 2")
         print(tau_y_2 + " vs " + tau_yield)
