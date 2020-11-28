471c5cf1b724622f358dcdf5f9a34ca5ffc8807d
justinbos003 <74308100+justinbos003@users.noreply.github.com>
2020-11-28 11:39:08 +0100

    comments to push check/fail for readability

--- push_fail.py
+++ push_fail.py
@@ -1,6 +1,6 @@
 from mat import pi
 from push_check.py import max_Fyi
-from  import Nf
+from     import Nf #imported from other code
 
 def get_sigma (Fy, A_zx):
     sigma_xz = Fy / A_zx  # normal stress due to Fyi
@@ -11,27 +11,32 @@ def get_tau (Fy, A_y):
     return tau_y
 
 Fy = max_Fyi #use maximum loaded fastener
+
+
+#values need to be put in still
 D_fo =
 D_fi =
-t2 =   #thickness from spacecraft wall
-t3 =   #thickness from lug wall
+t2 =      #thickness from spacecraft wall
+t3 =      #thickness from lug wall
+sigma_yield =     #depends on material type
+tau_yield =       #depends on material type
 
-A_xz = 1/4 * pi *(D_fo**2 - D_fi**2) #area normal stress works on
-A_y2 = pi * D_fi * t2 #area shear stress works on with thickness t4
-A_y3 =  pi * D_fi * t2 #area shear stress works on with thickness t3
+A_zx = 1/4 * pi *(D_fo**2 - D_fi**2) #area normal stress works on
+A_y2 = pi * D_fi * t2 #area shear stress works on with thickness t2
+A_y3 =  pi * D_fi * t2 #area shear stress works on with thickness t3 (shear stress is calculated for both plates on their own)
 
-for i in range(Nf):
-    sigma_xz = get_sigma(Fy, A_zx)
-    tau_y_2 = get_tau(Fy, A_y2)
-    tau_y_3 = get_tau(Fy, A_y3)
-    sigma_yield =
-    tau_yield =
-    if tau_y_2 > tau_yield:
-        print("for fastener " + i + " the shear stress is too high for thickness 2")
-        print(tau_y_2 + " vs " + tau_yield)
-    if tau_y_3 > tau_yield:
-        print("for fastener " + i + " the shear stress is too high for thickness 3")
-        print(tau_y_3 + " vs " + tau_yield)
+
+sigma_xz = get_sigma(Fy, A_zx) #calc norm stress
+tau_y_2 = get_tau(Fy, A_y2)    #calc shear stress for back plate
+tau_y_3 = get_tau(Fy, A_y3)    #calc shear stress for lug plate
+
+#check if shear stresses are too high
+if tau_y_2 > tau_yield:
+    print("the shear stress is too high for thickness 2")
+    print(tau_y_2 + " vs " + tau_yield)
+if tau_y_3 > tau_yield:
+    print("the shear stress is too high for thickness 3")
+    print(tau_y_3 + " vs " + tau_yield)
 
 
 
